e51308d4da3616bdfdb6279fbd2af59f209059ca
jackson <declipsonator@gmail.com>
2024-07-05 23:51:37 -0400

    Homepage

--- src/home.tsx
+++ src/home.tsx
@@ -1,19 +1,28 @@
 import './style/home.css'
 import moneyLogo from './assets/money.svg'
 import './style/index.css'
-
+import {Marquee} from './components/marquee'
 
 export function Home() {
+    const sponsors = <>hi</>;
 
     return (
+
+
         <>
             <div className="divImage">
                 <div id="topPageInfo">
                     <h3 className="fancy italic">A nonprofit for</h3>
                     <h1>Empowering low-income runners to pursue their passion</h1>
                     <h3 id="subtitle" className="fancy">Through grants and essential support</h3>
+                    <a href="https://en.wikipedia.org/wiki/World_domination" target="_blank" className="miniClickHere">OUR MISSION</a>
                 </div>
                 <img id="moneyImage" src={moneyLogo}  alt=""/>
+            </div>
+            <hr/>
+            <div id="sponsors">
+                <h2>Sponsors</h2>
+                {/*<Marquee input={sponsors}/>*/}
             </div>
         </>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,35 @@
 import {render} from 'preact'
 import {Home} from './home.tsx'
+import './style/main.css'
+import facebook from './assets/facebook.svg'
+import instagram from './assets/insta.svg'
+import twitter from './assets/twitter.svg'
 
-render(<Home/>, document.getElementById('app')!)
+const html =
+    <>
+        <div id="header">
+            <h1 id="headerTitle">strideforsuccess</h1>
+            <div id="headerLinks">
+                <a href="https://www.mcdonalds.com/">HOME</a>
+                <a href="https://www.airbnb.com/help">RUNNING GRANTS</a>
+                <a href="https://www.gofundme.com/f/bjzqz-help-me-buy-gaming-pc">DONATIONS</a>
+                <a href="https://247wallst.com/investing/2023/09/12/the-20-american-companies-with-the-worst-reputations/">SPONSORSHIPS</a>
+                <a href="https://www.clownfest.live/">OUR TEAM</a>
+            </div>
+        </div>
+
+        <Home/>
+
+        <div id="footer">
+            <h4 id="copyright">Copyright © Stride For Success Foundation</h4>
+            <div id="socialMedia">
+                <a href="https://www.facebook.com/"><img src={facebook} alt="facebook"/></a>
+                <a href="https://www.instagram.com/"><img src={instagram} alt="instagram"/></a>
+                <a href="https://www.twitter.com/"><img src={twitter} alt="twitter"/></a>
+            </div>
+        </div>
+    </>
+
+
+render(html, document.getElementById('app')!)
 
