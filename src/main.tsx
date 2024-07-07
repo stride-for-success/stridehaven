2bcceb150e8251da6774a67ebfcd266cda160416
jackson <declipsonator@gmail.com>
2024-07-06 21:33:48 -0400

    Start team page, switch to router, edit money, newsletter

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,60 @@
-import {render} from 'preact'
-import {Home} from './home.tsx'
-import './style/main.css'
-import facebook from './assets/facebook.svg'
-import instagram from './assets/insta.svg'
-import twitter from './assets/twitter.svg'
+import { render } from 'preact';
+import { Router, RouteComponentProps, Link } from "@reach/router";
+import { Home } from './home.tsx';
+// import { RunningGrants } from './runningGrants.tsx';
+// import { Donations } from './donations.tsx';
+// import { Sponsorships } from './sponsorships.tsx';
+import { Team } from './team.tsx';
+import './style/main.css';
+import facebook from './assets/facebook.svg';
+import instagram from './assets/insta.svg';
+import twitter from './assets/twitter.svg';
 
-const html =
+
+
+const App = () => (
     <>
         <div id="header">
             <h1 id="headerTitle">strideforsuccess</h1>
-            <div id="headerLinks">
-                <a href="https://www.mcdonalds.com/">HOME</a>
-                <a href="https://www.airbnb.com/help">RUNNING GRANTS</a>
-                <a href="https://www.gofundme.com/f/bjzqz-help-me-buy-gaming-pc">DONATIONS</a>
-                <a href="https://247wallst.com/investing/2023/09/12/the-20-american-companies-with-the-worst-reputations/">SPONSORSHIPS</a>
-                <a href="https://www.clownfest.live/">OUR TEAM</a>
-            </div>
+            <nav id="headerLinks">
+                <Link to="/stridehaven/">HOME</Link>
+                <Link to="/stridehaven/running-grants">RUNNING GRANTS</Link>
+                <Link to="/stridehaven/donations">DONATIONS</Link>
+                <Link to="/stridehaven/sponsorships">SPONSORSHIPS</Link>
+                <Link to="/stridehaven/our-team">OUR TEAM</Link>
+            </nav>
         </div>
 
-        <Home/>
+        <Router basepath="/stridehaven">
+            <RouterPage path="/" pageComponent={<Home />} />
+            <RouterPage path="/our-team" pageComponent={<Team />} />
+        </Router>
 
         <div id="footer">
             <h4 id="copyright">Copyright © Stride For Success Foundation</h4>
+            <div id="newsletter">
+                <input type="email" id="newsletterEmail" placeholder="Email" />
+                <button id="subscribe" onClick={subscribeToNewsletter}>Subscribe</button>
+            </div>
             <div id="socialMedia">
-                <a href="https://www.facebook.com/" target="_blank"><img src={facebook} alt="facebook"/></a>
-                <a href="https://www.instagram.com/" target="_blank"><img src={instagram} alt="instagram"/></a>
-                <a href="https://www.twitter.com/" target="_blank"><img src={twitter} alt="twitter"/></a>
+                <a href="https://www.facebook.com/" target="_blank"><img src={facebook} alt="facebook" /></a>
+                <a href="https://www.instagram.com/" target="_blank"><img src={instagram} alt="instagram" /></a>
+                <a href="https://www.twitter.com/" target="_blank"><img src={twitter} alt="twitter" /></a>
             </div>
         </div>
     </>
+);
 
+const RouterPage = (
+    props: { pageComponent: JSX.Element } & RouteComponentProps
+) => props.pageComponent;
 
-render(html, document.getElementById('app')!)
+render(<App />, document.getElementById('app')!);
 
+async function subscribeToNewsletter() {
+    const email = (document.getElementById('newsletterEmail') as HTMLInputElement).value;
+    console.log(`Subscribed ${email} to newsletter`);
+    (document.getElementById('newsletterEmail') as HTMLInputElement).value = 'Subscribed!';
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    (document.getElementById('newsletterEmail') as HTMLInputElement).value = '';
+}
---
+++ src/team.tsx
@@ -0,0 +1,64 @@
+import './style/home.css'
+import './style/index.css'
+import moneyLogo from './assets/money.svg'
+import random from './assets/random.svg'
+import trackStuff from './assets/trackStuff.svg'
+import donation from './assets/donation.svg'
+
+
+export function Team() {
+    const marqueeElements = Array.from({length: 10}, (_, i) =>
+        <div className="marqueeInside" key={i}>
+            <a href="" target="_blank"> <img src={random} alt="track stuff"/> </a>
+        </div>);
+
+    return (
+
+
+        <>
+            <div className="divImage">
+                <div id="topPageInfo">
+                    <h3 className="fancy italic">A nonprofit for</h3>
+                    <h1>Empowering low-income runners to pursue their passion</h1>
+                    <h3 id="subtitle" className="fancy">Through grants and essential support</h3>
+                    <a href="https://en.wikipedia.org/wiki/World_domination" target="_blank" className="miniClickHere">OUR
+                        MISSION</a>
+                </div>
+                <img id="moneyImage" src={moneyLogo} alt=""/>
+            </div>
+            <br/>
+            <hr/>
+            <div id="sponsors">
+                <h2 id="sponsorsText">Sponsors</h2>
+                <div className="marquee">
+                    {marqueeElements}
+                </div>
+            </div>
+            <hr/>
+            <br/>
+            <div className="divImage">
+                <img id="trackStuff" src={trackStuff} alt=""/>
+                <div id="about">
+                    <h1>Why</h1>
+                    <h3 className="fancy italic">50% of U.S. track athletes who rank in the Top 10 in their event made less than $15,000 annually from their sport as of 2014.</h3>
+                    <p id="aboutParagraph">
+                        Runners deserve to be able to pursue their passion without worrying about financial constraints. We provide grants to low-income runners to help them achieve their dreams.
+                    </p>
+                </div>
+            </div>
+            <hr/>
+            <br/>
+            <div className="divImage">
+                <div id="support">
+                    <h1>Support Us</h1>
+                    <p id="donationParagraph">
+                        Stride For Success Foundation is a 501(c)(3) nonprofit organization. Your donation is tax-deductible. We are a 100% volunteer-run, so your donation goes directly to supporting our mission.
+                    </p>
+
+                    <a href="https://www.gofundme.com/f/bjzqz-help-me-buy-gaming-pc" target="_blank" className="miniClickHere">DONATE</a></div>
+                <img id="donation" src={donation} alt=""/>
+            </div>
+
+        </>
+    )
+}
